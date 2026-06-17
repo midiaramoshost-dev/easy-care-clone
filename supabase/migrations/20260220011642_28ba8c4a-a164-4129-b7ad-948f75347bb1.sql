c05003e8b8715170256e2599ab276ee944483b3f
midiaramoshost-dev <midiaramoshost@gmail.com>
2026-06-17 22:57:31 +0000

    Deploying to stable-website from @ midiaramoshost-dev/easy-care-clone@ffbd133ca5de7ccccb9e694c16a9caf1a8263416 🚀

--- supabase/migrations/20260220011642_28ba8c4a-a164-4129-b7ad-948f75347bb1.sql
+++
@@ -1,5 +0,0 @@
--- Allow admins to update donations (for confirming payments)
-CREATE POLICY "Admins can update donations"
-  ON public.donations
-  FOR UPDATE
-  USING (has_role(auth.uid(), 'admin'::app_role));
